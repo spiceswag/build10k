8a5b297442a7eee43e66dffa50ac1dc0cf8b1b50
spicesw <spicesw@proton.me>
2026-06-20 23:02:22 +0300

    permit root login

--- src/system.nix
+++ src/system.nix
@@ -28,7 +28,6 @@
   services.openssh = {
     enable = true;
     settings = {
-      PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
   };
--- src/users.nix
+++ src/users.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 {
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjyDnNQvxO+Xj4Jp42F67LmOAiHrQtJwhwVPkDHQ3Ep spicesw@proton.me"
+    ];
+  };
+
   users.users.spice = {
     description = "ταφ";
     hashedPassword = "$y$j9T$ESLpEbpQbXte5fXg7bjsq0$9Zo41KD0/A6a3DliR58plTKv8ryaLnOGdpESKOjn0wB";
