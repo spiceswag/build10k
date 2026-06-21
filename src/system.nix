13df33ad76e42c69c4988cceb61924b735631626
spicesw <spicesw@proton.me>
2026-06-21 10:20:52 +0300

    add correct virtualization settings

--- src/system.nix
+++ src/system.nix
@@ -1,6 +1,23 @@
-{ lib, inputs, ... }:
 {
-  boot.isContainer = true;
+  lib,
+  inputs,
+  modulesPath,
+  ...
+}:
+{
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = false;
+  };
+  services.fstrim.enable = false; # Let Proxmox host handle fstrim
+  # Cache DNS lookups to improve performance
+  services.resolved = {
+    extraConfig = ''
+      Cache=true
+      CacheFromLocalhost=true
+    '';
+  };
 
   nix = {
     settings.experimental-features = [
