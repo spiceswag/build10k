4e1a9397b3282650d0de0bb3b439d57f7644dc8b
spicesw <spicesw@proton.me>
2026-06-21 13:49:15 +0300

    apparently resolved doesnt do caching anymore

--- src/system.nix
+++ src/system.nix
@@ -11,13 +11,6 @@
     privileged = false;
   };
   services.fstrim.enable = false; # Let Proxmox host handle fstrim
-  # Cache DNS lookups to improve performance
-  services.resolved = {
-    extraConfig = ''
-      Cache=true
-      CacheFromLocalhost=true
-    '';
-  };
 
   nix = {
     settings.experimental-features = [
